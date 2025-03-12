2187efc4791c6080ccc8601fa071187b1f107988
ShamansGit <56842156+ShamansGit@users.noreply.github.com>
2025-03-12 19:43:55 +0000

    Added basic server functionality
    Controller website can now be run locally on the host machine, although networking across LAN isn't working yet.

--- server.py
+++ server.py
@@ -1,5 +1,27 @@
 #Code for handling setting up a local LAN server and networking etc.
 #Being worked on by: Joe
 
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+ADDRESS = ("0.0.0.0", 8765)
+
 def start_server():
-    pass
+    print("Starting Server...")
+    httpd = HTTPServer(ADDRESS,Serv)
+    httpd.serve_forever()
+
+class Serv(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+       if self.path == '/':
+           self.path = '/Website/bobert.html'
+       try:
+           file_to_open = open(self.path[1:]).read()
+           self.send_response(200)
+       except:
+           file_to_open = "File not found"
+           self.send_response(404)
+       self.end_headers()
+       self.wfile.write(bytes(file_to_open, 'utf-8'))
+
+start_server()
