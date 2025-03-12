7c7532f22479a9c252782c329d3b96bda83a5675
ShamansGit <56842156+ShamansGit@users.noreply.github.com>
2025-03-12 20:03:38 +0000

    Added comments and proper indent

--- server.py
+++ server.py
@@ -3,6 +3,7 @@
 
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
+#0.0.0.0 will make the website available for anyone on the local network on port 8765
 ADDRESS = ("0.0.0.0", 8765)
 
 def start_server():
@@ -10,18 +11,23 @@ def start_server():
     httpd = HTTPServer(ADDRESS,Serv)
     httpd.serve_forever()
 
+#this class handles the HTTP server functionality
 class Serv(BaseHTTPRequestHandler):
-
     def do_GET(self):
-       if self.path == '/':
-           self.path = '/Website/bobert.html'
-       try:
-           file_to_open = open(self.path[1:]).read()
-           self.send_response(200)
-       except:
-           file_to_open = "File not found"
-           self.send_response(404)
-       self.end_headers()
-       self.wfile.write(bytes(file_to_open, 'utf-8'))
+        if self.path == '/':
+            #sets default path to main page
+            self.path = '/Website/bobert.html'
+        try:
+            #reads the HTML file and sends a response code of 200 (success)
+            file_to_open = open(self.path[1:]).read()
+            self.send_response(200)
+        except:
+            #send 404 response code otherwise
+            file_to_open = "Error 404: File not found."
+            self.send_response(404)
+        #send file back to client
+        self.end_headers()
+        self.wfile.write(bytes(file_to_open, 'utf-8'))
 
+#placeholder! This will run from main.py in the future.
 start_server()
